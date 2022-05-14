23705e8a4631abd90e629a07a375eaa908bcbf02
Flex01a <91494774+Flex01a@users.noreply.github.com>
2022-05-14 20:21:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,24 @@
 import requests
 from flask import Flask
 
-def send_msg(text):
-   token = "1883871949:AAHU-dHbSvQhalGIHTebf1x19llaqqK133o"
-   chat_id = "614135975"
+def send_msg(text,toke,chatid):
+   token = toke
+   chat_id = chatid
    url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text 
    results = requests.get(url_req)
    print(results.json())
 
-send_msg("Hello there!")
+s1 = input("BotToken : ")
+
+s2 = input(" ChatID : ")
+
+s3 = input(" Message : ") 
+
+
+
+
+
+send_msg(s3,s1,s3)
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 8519)
